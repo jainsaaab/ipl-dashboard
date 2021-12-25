8ba224c28f558980b11013436a2699a4fbf6ba66
Aditya Jain <jainsaab@protonmail.com>
2021-12-25 23:14:12 +0530

    link for MatchDetailCard

--- src/frontend/src/components/MatchDetailCard.js
+++ src/frontend/src/components/MatchDetailCard.js
@@ -1,4 +1,5 @@
 import { React } from 'react';
+import { Link } from 'react-router-dom';
 
 export const MatchDetailCard = ({ match, teamName }) => {
     if (!match) return null;
@@ -8,7 +9,7 @@ export const MatchDetailCard = ({ match, teamName }) => {
     return (
         <div className="MatchDetailCard">
             <h3>Latest Matches</h3>
-            <h1>vs {otherTeam}</h1>
+            <h1>vs <Link to={"/teams/" + otherTeam}>{otherTeam}</Link></h1>
             <h2>{match.date}</h2>
             <h3>at {match.venue}</h3>
             <h3>{match.matchWinner} won by {match.resultMargin} {match.result}</h3>
--- src/frontend/src/components/MatchSmallCard.js
+++ src/frontend/src/components/MatchSmallCard.js
@@ -9,8 +9,7 @@ export const MatchSmallCard = ({ match, teamName }) => {
 
     return (
         <div className="MatchSmallCard">
-            <h3>vs 
-                <Link to={"/teams/"+otherTeam}>{otherTeam}</Link></h3>
+            <h3>vs <Link to={"/teams/"+otherTeam}>{otherTeam}</Link></h3>
             <p>{match.matchWinner} won by {match.resultMargin} {match.result}</p>
         </div>
     );
